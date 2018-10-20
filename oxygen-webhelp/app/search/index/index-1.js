9cd4c2cdee15d4647dbd654f764ba6bf4d446eef
oXygen XML Deployer <support@oxygenxml.com>
2018-10-20 16:31:44 +0000

    rebuild pages at 0fc4cde

--- oxygen-webhelp/app/search/index/index-1.js
+++ oxygen-webhelp/app/search/index/index-1.js
@@ -1,4 +1,4 @@
 define(function () {
 // Auto generated index for searching.
-return {"...":"0*9*-1","are:":"0*10*-1$f","attribute":"0*0*15","avoidattributeinelement":"0*5*12","content":"0*0*3","dita":"0*0*1c","do":"0*0*18","documentation":"0*10*-1$e","dynamically":"0*10*-1$10","dynamically.":"0*10*-1$v","edit":"0*10*6","element":"0*0*13"};
+return {"...":"0*9*-1","@scale":"0*0*12","application":"0*0*1i","are:":"0*10*-1$f","attribute":"0*1*14$1t","avoidattributein":"0*5*1p","content":"0*0*3","different":"0*0*1c","dita":"0*0*24","do":"0*0*20","documentation":"0*10*-1$e","dynamically":"0*10*-1$10","dynamically.":"0*10*-1$v","edit":"0*10*6","element":"0*6*19$1q$1r","element.":"0*0*18"};
 });
