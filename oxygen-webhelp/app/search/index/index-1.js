f11d92ece97bfcde0cd3e37e99c2a8d15ad430c9
oXygen XML Deployer <support@oxygenxml.com>
2018-10-22 20:35:42 +0000

    rebuild pages at 7251973

--- oxygen-webhelp/app/search/index/index-1.js
+++ oxygen-webhelp/app/search/index/index-1.js
@@ -1,4 +1,4 @@
 define(function () {
 // Auto generated index for searching.
-return {"...":"0*9*-1","@scale":"0*0*12","application":"0*0*1i","are:":"0*10*-1$f","attribute":"0*1*14$1s","avoidattributeinelement":"0*5*1p","content":"0*0*3","different":"0*0*1c","dita":"0*0*22","do":"0*0*1v","documentation":"0*10*-1$e","dynamically":"0*10*-1$10","dynamically.":"0*10*-1$v","edit":"0*10*6","element":"0*1*19$1q","element.":"0*0*18"};
+return {"...":"0*9*-1","@scale":"0*0*12","application":"0*0*1i","are:":"0*10*-1$f","attribute":"0*0*14","content":"0*0*3","different":"0*0*1c","documentation":"0*10*-1$e","dynamically":"0*10*-1$10","dynamically.":"0*10*-1$v","edit":"0*10*6","element":"0*0*19","element.":"0*0*18","formats":"0*38*-1$-1$-1$9$b$p"};
 });
--- oxygen-webhelp/app/search/index/index-2.js
+++ oxygen-webhelp/app/search/index/index-2.js
@@ -1,4 +1,4 @@
 define(function () {
 // Auto generated index for searching.
-return {"formats":"0*38*-1$-1$-1$9$b$p","formats.":"0*10*-1$o","image":"0*60*-1$-1$-1$-1$8$a$n$q$17$1d$1g$1r","images":"0*41*-1$0$5$s$21","jpg":"0*10*-1$h","jump":"0*0*1","main":"0*0*2","means":"0*10*-1$11","message":"0*0*1u","need":"0*0*1b","obtain":"0*0*1j","online":"0*10*7","other":"0*10*-1$m","png":"0*10*-1$g","processing":"0*0*1h","rule":"0*5*1o"};
+return {"formats.":"0*10*-1$o","image":"0*59*-1$-1$-1$-1$8$a$n$q$17$1d$1g","images":"0*40*-1$0$5$s","jpg":"0*10*-1$h","jump":"0*0*1","main":"0*0*2","means":"0*10*-1$11","need":"0*0*1b","obtain":"0*0*1j","online":"0*10*7","other":"0*10*-1$m","png":"0*10*-1$g","processing":"0*0*1h","scale":"0*0*13"};
 });
