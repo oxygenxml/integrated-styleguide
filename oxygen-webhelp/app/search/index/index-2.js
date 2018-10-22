163a6340f47c176a7ec4e4e4ccb8ee7a3cce6ced
oXygen XML Deployer <support@oxygenxml.com>
2018-10-22 21:42:21 +0000

    rebuild pages at 3263644

--- oxygen-webhelp/app/search/index/index-2.js
+++ oxygen-webhelp/app/search/index/index-2.js
@@ -1,4 +1,4 @@
 define(function () {
 // Auto generated index for searching.
-return {"formats.":"0*10*-1$o","image":"0*59*-1$-1$-1$-1$8$a$n$q$17$1d$1g","images":"0*40*-1$0$5$s","jpg":"0*10*-1$h","jump":"0*0*1","main":"0*0*2","means":"0*10*-1$11","need":"0*0*1b","obtain":"0*0*1j","online":"0*10*7","other":"0*10*-1$m","png":"0*10*-1$g","processing":"0*0*1h","scale":"0*0*13"};
+return {"formats.":"0*10*-1$o","image":"0*60*-1$-1$-1$-1$8$a$n$q$17$1d$1g$1r","images":"0*40*-1$0$5$s","jpg":"0*10*-1$h","jump":"0*0*1","main":"0*0*2","means":"0*10*-1$11","message":"0*1*1u$20","message...":"0*0*1v","need":"0*0*1b","obtain":"0*0*1j","online":"0*10*7","other":"0*10*-1$m","png":"0*10*-1$g","processing":"0*0*1h"};
 });
