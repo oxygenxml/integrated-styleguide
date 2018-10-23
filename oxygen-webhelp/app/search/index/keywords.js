e67a20eee0002850a9ee353c55add72f75ed62af
oXygen XML Deployer <support@oxygenxml.com>
2018-10-23 02:40:15 +0000

    rebuild pages at f5cf45f

--- oxygen-webhelp/app/search/index/keywords.js
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -1,7 +1,8 @@
-define(function() {var keywords=[{w:"Images",p:["p0"]},{w:"Lists",p:["p1"]}];
+define(function() {var keywords=[{w:"Images",p:["p0"]},{w:"Lists",p:["p1"]},{w:"Metadata",p:["p2"]}];
 var ph={};
 ph["p0"]=[0];
 ph["p1"]=[1];
+ph["p2"]=[2];
      return {
          keywords: keywords,
          ph: ph
